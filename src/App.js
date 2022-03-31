7873462a3279a063cc861edbb31842044cd781fe
Emanuelit <emanuelitavelino@gmail.com>
2022-03-30 21:55:16 -0300

    Atualizando projeto

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './app.css';
 export default function App(){
 const[peso, setPeso] = useState('');
 const[altura, setAltura] = useState('');
-
-function calcularIMC(){
+const [mensagem, setMensagem] = useState('');
+  function calcularIMC(){
   const alt = altura / 100;
   const imc = peso / (alt * alt);
 
-  const [mensagem, setMensagem] = useState('Alguma mensagem');
+  
 if(imc < 18.6){
   setMensagem("Você está abaixo do peso! Seu IMC: " +imc.toFixed(2))} else if(imc >= 18.6 && imc <24.9){
     setMensagem("Peso ideal! Seu IMC: " + imc.toFixed(2))
@@ -19,34 +19,35 @@ if(imc < 18.6){
     setMensagem("Cuidado Obesidade! Seu IMC: " +  imc.toFixed(2))
   }
 
-  return(
-    <div className="app">
-      <h1>Calculadora IMC</h1>
-      <span>Vamos calcular seu imc</span>
-
-      <div className="area-input">
-        <input
-        type="text"
-        placeholder="Peso em (kg) Ex: 90"
-        value={peso}
-        onChange={(e) => setPeso(e.target.value)}
-        />
-        
-        <input
-        type="text"
-        placeholder="Altura em (cm) Ex: 190"
-        value={altura}
-        onChange={(e) => setAltura(e.target.value)}
-        />
-
-        <button onClick={calcularIMC}>
-          Calcular
-        </button>
-      </div>
-
-      <h2>{mensagem}</h2>
-
-    </div>
-  )
+  
 }
+return(
+  <div className="app">
+    <h1>Calculadora IMC</h1>
+    <span>Vamos calcular seu imc</span>
+
+    <div className="area-input">
+      <input
+      type="text"
+      placeholder="Peso em (kg) Ex: 90"
+      value={peso}
+      onChange={(e) => setPeso(e.target.value)}
+      />
+      
+      <input
+      type="text"
+      placeholder="Altura em (cm) Ex: 190"
+      value={altura}
+      onChange={(e) => setAltura(e.target.value)}
+      />
+
+      <button onClick={calcularIMC}>
+        Calcular
+      </button>
+    </div>
+
+    <h2>{mensagem}</h2>
+
+  </div>
+)
 }
